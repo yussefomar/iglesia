f19c47e5fca5320413effc7c6dda40eba20b2935
yussefomar <aprocalot@gmail.com>
2017-08-15 18:21:42 -0300

    primeraprueba

---
+++ iglesia/src/EMM/UserBundle/Controller/UserController.php
@@ -0,0 +1,375 @@
+<?php
+namespace EMM\UserBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\FormError;
+use EMM\UserBundle\Entity\User;
+use EMM\UserBundle\Form\UserType;
+use EMM\USerBundle\Models\Document;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use EMM\UserBundle\Entity\Archivo;
+
+
+class UserController extends Controller
+{
+    public function aboutAsAction(Request $request){
+        
+        
+        
+           $repositorio=$this->getDoctrine()->getRepository('EMMUserBundle:Archivo');
+                         $query=$repositorio->createQueryBuilder('a')->orderBy('a.createdAt','DESC')->setMaxResults(10)->getQuery();
+              
+                         $archivos=$query->getResult();
+        
+        if ($request->getMethod() == 'POST') {
+            $image = $request->files->get('archivito');
+
+            if (($image instanceof UploadedFile) && ($image->getError() == '0'))/* image es un instancia de uploadfile  y si no hay la crea   y el get error es igual a cero osea estatus recicibiod bien, pero todavia no se  guarda */ {
+                if (($image->getSize() < 2000000000)) {
+                    $originalName = $image->getClientOriginalName(); /* retorna el nombre del archivo original */
+
+                    $name_array = explode('.', $originalName); /* separa los elemento que tengan un punto */
+                    $file_type = $name_array[sizeof($name_array) - 1]; /* obtiene la extension del largo menos uno, me duevle la extension ya que empieza de cero el arreglo y termina en sizeof menos uno */
+                    $valid_filetypes = array('jpg', 'jpeg', 'png','mp3'); /* crea un arreglo con todas las extenciones que vas a aceptar, valida que estos suban esos tipos de archivos */
+                    if (in_array(strtolower($file_type), $valid_filetypes))/* valida si tiene alguna de esas extensiones, in_array es para de php, sirve para buscarelementos denro de un arreglo, y strlower para pasar a mayuscula, filetyep para recorrelo , si retorna verdadero entonces cumple */ {
+                        $document = new Document();
+                        $document->setFile($image); /* recibe como parametro el arreglo y con es hace el upload delarchivo */
+                        $document->setSubDirectory('archivos'); /* dentro dela caparpeta upload puedo llamar una carpeta con esenombre,podrias tener carpeta para los usuarios,arcvhiso/ y algo siqueires mas */
+                        $document->processFile(); /**/
+                        
+                        $usuarios= $this->get('security.token_storage')->getToken()->getUser();
+                        
+                       
+                        
+                        $archivo = new Archivo();
+                        $archivo->setArchivo($image->getBasename()); /*  getbasenameese metodo se almancena el nombre con que symfony lo guardo en la carpeta .tmp */
+                        $archivo->setTitle('yussef');
+                        
+                         //relate this product to the category
+                        
+                        $archivo->setUser($usuarios);
+                      
+                        $archivo->setStatus(1);
+                        $archivo->setDescription('nuevo archivo');
+                        
+                       
+                        
+                         
+                       
+                        $em = $this->getDoctrine()->getManager();
+                        $em->persist($usuarios);
+                        $em->persist($archivo);
+                        $em->flush();
+                        $this->get('session')->getFlashBag()->add(
+                                'mensaje', 'el archivo se ha subido correctamente'
+                        );
+                        
+                        
+                         
+        
+                      return $this->render('ProyectoUserBundle:User:aboutAs.html.twig',array('archivos'=>$archivos));
+                        
+                        
+                        
+                        
+                         
+                    } else {
+                        $this->get('session')->getFlashBag()->add(
+                                'mensaje', 'la extensión del archivo no es la correcta'
+                        );
+                        //redirecciono        
+                        return $this->redirect($this->generateUrl('emm_user_aboutas'));
+                    }
+                }
+            } else {
+                $this->get('session')->getFlashBag()->add(
+                        'mensaje', 'no entró o se produjo algún error inesoperado'
+                );
+                //redirecciono        
+                return $this->redirect($this->generateUrl('emm_user_aboutas'));
+                //die("no entró o se produjo algún error inesoperado");
+            }
+        }
+         return $this->render('EMMUserBundle:User:aboutAs.html.twig',array('archivos'=>$archivos));
+    }
+    
+    
+    
+    public function sermonesAction(){
+         return $this->render('EMMUserBundle:User:home.html.twig');
+    }
+    
+    public function homeAction(){
+        return $this->render('EMMUserBundle:User:home.html.twig');
+    }
+    public function indexAction(Request $request)
+    {
+        
+        $em = $this->getDoctrine()->getManager();
+        
+        // $users = $em->getRepository('EMMUserBundle:User')->findAll();
+        
+        /*
+        
+        $res = 'Lista de usuarios: <br />';
+        
+        foreach($users as $user)
+        {
+            $res .= 'Usuario: ' . $user->getUsername() . ' - Email: ' . $user->getEmail() . '<br />';
+        }
+        
+        return new Response($res);
+        */
+        
+        $dql = "SELECT u FROM EMMUserBundle:User u ORDER BY u.id DESC";
+        $users = $em->createQuery($dql);
+        
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $users, $request->query->getInt('page', 1),
+            10
+        );
+        
+        $deleteFormAjax = $this->createCustomForm(':USER_ID', 'DELETE', 'emm_user_delete');
+        
+        return $this->render('EMMUserBundle:User:index.html.twig', array('pagination' => $pagination, 'delete_form_ajax' => $deleteFormAjax->createView()));
+    }
+    
+    public function addAction()
+    {
+        $user = new User();
+        $form = $this->createCreateForm($user);
+        
+        return $this->render('EMMUserBundle:User:add.html.twig', array('form' => $form->createView()));
+    }
+    
+    private function createCreateForm(User $entity)
+    {
+        $form = $this->createForm(new UserType(), $entity, array(
+                'action' => $this->generateUrl('emm_user_create'),
+                'method' => 'POST'
+            ));
+        
+        return $form;
+    }
+    
+    public function createAction(Request $request)
+    {   
+        $user = new User();
+        $form = $this->createCreateForm($user);
+        $form->handleRequest($request);
+        
+        if($form->isValid())
+        {
+            $password = $form->get('password')->getData();
+            
+            $passwordConstraint = new Assert\NotBlank();
+            $errorList = $this->get('validator')->validate($password, $passwordConstraint);
+            
+            if(count($errorList) == 0)
+            {
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $password);
+                
+                $user->setPassword($encoded);
+                
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+                
+                $successMessage = $this->get('translator')->trans('The user has been created.');
+                $this->addFlash('mensaje', $successMessage);
+                
+                return $this->redirectToRoute('emm_user_index');                
+            }
+            else
+            {
+                $errorMessage = new FormError($errorList[0]->getMessage());
+                $form->get('password')->addError($errorMessage);
+            }
+        }
+        
+        return $this->render('EMMUserBundle:User:add.html.twig', array('form' => $form->createView()));
+    }
+    
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('EMMUserBundle:User')->find($id);
+        
+        if(!$user)
+        {
+            $messageException = $this->get('translator')->trans('User not found.');
+            throw $this->createNotFoundException($messageException);
+        }
+        
+        $form = $this->createEditForm($user);
+        
+        return $this->render('EMMUserBundle:User:edit.html.twig', array('user' => $user, 'form' => $form->createView()));
+        
+    }
+    
+    private function createEditForm(User $entity)
+    {
+        $form = $this->createForm(new UserType(), $entity, array('action' => $this->generateUrl('emm_user_update', array('id' => $entity->getId())), 'method' => 'PUT'));
+        
+        return $form;
+    }
+    
+    public function updateAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $user = $em->getRepository('EMMUserBundle:User')->find($id);
+        if(!$user)
+        {
+            $messageException = $this->get('translator')->trans('User not found.');
+            throw $this->createNotFoundException($messageException);
+        }
+        
+        $form = $this->createEditForm($user);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $password = $form->get('password')->getData();
+            if(!empty($password))
+            {
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $password);
+                $user->setPassword($encoded);
+            }
+            else
+            {
+                $recoverPass = $this->recoverPass($id);
+                $user->setPassword($recoverPass[0]['password']);                
+            }
+            
+            if($form->get('role')->getData() == 'ROLE_ADMIN')
+            {
+                $user->setIsActive(1);
+            }
+            $em->flush();
+            
+            $successMessage = $this->get('translator')->trans('The user has been modified.');
+            $this->addFlash('mensaje', $successMessage);
+            return $this->redirectToRoute('emm_user_edit', array('id' => $user->getId()));
+        }
+        return $this->render('EMMUserBundle:User:edit.html.twig', array('user' => $user, 'form' => $form->createView()));
+    }
+    
+    private function recoverPass($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT u.password
+            FROM EMMUserBundle:User u
+            WHERE u.id = :id'    
+        )->setParameter('id', $id);
+        
+        $currentPass = $query->getResult();
+        
+        return $currentPass;
+    }
+    
+    public function viewAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository('EMMUserBundle:User');
+        
+        $user = $repository->find($id);
+        
+        if(!$user)
+        {
+            $messageException = $this->get('translator')->trans('User not found.');
+            throw $this->createNotFoundException($messageException);
+        }
+        
+        $deleteForm = $this->createCustomForm($user->getId(), 'DELETE', 'emm_user_delete');
+        
+        return $this->render('EMMUserBundle:User:view.html.twig', array('user' => $user, 'delete_form' => $deleteForm->createView()));
+    }
+    
+    public function deleteAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $user = $em->getRepository('EMMUserBundle:User')->find($id);
+        
+        if(!$user)
+        {
+            $messageException = $this->get('translator')->trans('User not found.');
+            throw $this->createNotFoundException($messageException);
+        }
+        
+        $allUsers = $em->getRepository('EMMUserBundle:User')->findAll();
+        $countUsers = count($allUsers);
+        
+        // $form = $this->createDeleteForm($user);
+        $form = $this->createCustomForm($user->getId(), 'DELETE', 'emm_user_delete');
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid())
+        {
+            if($request->isXMLHttpRequest())
+            {
+                $res = $this->deleteUser($user->getRole(), $em, $user);
+                
+                return new Response(
+                    json_encode(array('removed' => $res['removed'], 'message' => $res['message'], 'countUsers' => $countUsers)),
+                    200,
+                    array('Content-Type' => 'application/json')
+                );
+            }
+            
+            $res = $this->deleteUser($user->getRole(), $em, $user);
+            $this->addFlash($res['alert'], $res['message']);
+            return $this->redirectToRoute('emm_user_index');            
+        }
+    }
+    
+    private function deleteUser($role, $em, $user)
+    {
+        if($role == 'ROLE_USER')
+        {
+            $em->remove($user);
+            $em->flush();
+            
+            $message = $this->get('translator')->trans('The user has been deleted.');
+            $removed = 1;
+            $alert = 'mensaje';
+        }
+        elseif($role == 'ROLE_ADMIN')
+        {
+            $message = $this->get('translator')->trans('The user could not be deleted.');
+            $removed = 0;
+            $alert = 'error';
+        }
+        
+        return array('removed' => $removed, 'message' => $message, 'alert' => $alert);
+    }
+    
+    private function createCustomForm($id, $method, $route)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl($route, array('id' => $id)))
+            ->setMethod($method)
+            ->getForm();
+    }
+    
+    
+    
+    public function plantillasVentaAction(){
+        
+        
+        return $this->render('EMMUserBundle:User:plantillasVenta.html.twig');
+    }
+    
+    
+    public function subirAction(Request $request){
+        
+        
+          return $this->render('EMMUserBundle:User:subir.html.twig');
+    }    
+}
---
+++ iglesia/src/EMM/UserBundle/Entity/Archivo.php
@@ -0,0 +1,278 @@
+<?php
+
+namespace EMM\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Archivo
+ *
+ * @ORM\Table(name="archivo")
+ * @ORM\Entity(repositoryClass="EMM\UserBundle\Repository\ArchivoRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Archivo
+{
+    
+     /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="archivos")
+     * @ORM\JoinColumn(name="userid", referencedColumnName="id", onDelete="CASCADE")
+     */ 
+    
+     protected $user;
+    
+    
+    
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="status", type="boolean")
+     */
+    private $status;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="archivo", type="string", length=255)
+     */
+    private $archivo;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Archivo
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Archivo
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set status
+     *
+     * @param boolean $status
+     * @return Archivo
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return boolean 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    
+     
+    
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Archivo
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    
+    
+   
+    
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Archivo
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    
+    
+    public function setUpdatedAtValue()/* fecha la hora correspondiente ala que se va a tuilizar*/
+    {
+        $this->updatedAt =new \DateTime();/* esto se setea en prepersist y tambien adiciamente preudate en el cmomento que actualizemos o modifquemos uno de nuestros registrso*/
+
+        return $this;
+    }
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreatedAtValue( )/*va a sestiar nuestra fecha actual a la que se esta registrarse*/
+    {
+        $this->createdAt = new \DateTime();/*al momento qaue perssiste , setea por eso prepersisit*/
+
+        return $this;
+    }
+    
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set archivo
+     *
+     * @param string $archivo
+     * @return Archivo
+     */
+    public function setArchivo($archivo)
+    {
+        $this->archivo = $archivo;
+
+        return $this;
+    }
+
+    /**
+     * Get archivo
+     *
+     * @return string 
+     */
+    public function getArchivo()
+    {
+        return $this->archivo;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \EMM\UserBundle\Entity\User $user
+     * @return Archivo
+     */
+    public function setUser(\EMM\UserBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \EMM\UserBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
